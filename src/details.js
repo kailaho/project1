931519b09db27c9415272f4082e0f5dbdaf099c0
kailaho <kaho3670@colorado.edu>
2023-11-02 10:54:57 -0600

    trying a different file path

--- src/details.js
+++ src/details.js
@@ -1,5 +1,5 @@
 
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three/build/three.js';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 
